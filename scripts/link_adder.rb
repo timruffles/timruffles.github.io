e15332d6233597657e5444a19105ab3822b6354d
Tim Ruffles <timruffles@googlemail.com>
2013-10-09 11:05:38 +0100

    update

--- scripts/link_adder.rb
+++ scripts/link_adder.rb
@@ -8,7 +8,7 @@ loop do
   puts "Ok to expose (y/n): /l/#{slug}, pointing at #{url}"
   yes = gets.chomp
   if yes.downcase == "y"
-    db.exec_params("INSERT INTO links (name,url) VALUES ('$1','$2')",[slug,url])
+    db.exec_params("INSERT INTO links (name,url) VALUES ($1,$2)",[slug,url])
     puts "Done!\n"
   else
     puts "Ignoring\n"
