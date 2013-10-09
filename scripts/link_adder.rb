65760c68743b06a57ea621e8741aaaae343ef8cd
Tim Ruffles <timruffles@googlemail.com>
2013-10-09 10:57:42 +0100

    chompy

--- scripts/link_adder.rb
+++ scripts/link_adder.rb
@@ -2,11 +2,11 @@ require_relative "../db"
 db = get_db
 loop do
   puts "Add link url:"
-  url = gets
+  url = gets.chomp
   puts "Add link slug:"
-  slug = gets
+  slug = gets.chomp
   puts "Ok to expose (y/n): /l/#{slug}, pointing at #{url}"
-  yes = gets
+  yes = gets.chomp
   if yes.downcase = "y"
     db.exec_params("INSERT INTO links (name,url) VALUES ('$1','$2')",[slug,url])
     puts "Done!\n"
