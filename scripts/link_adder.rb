73ae0605be965b5f302873ec89ba7b6d2add0f28
Tim Ruffles <timruffles@googlemail.com>
2013-10-09 10:59:11 +0100

    hah

--- scripts/link_adder.rb
+++ scripts/link_adder.rb
@@ -7,7 +7,7 @@ loop do
   slug = gets.chomp
   puts "Ok to expose (y/n): /l/#{slug}, pointing at #{url}"
   yes = gets.chomp
-  if yes.downcase = "y"
+  if yes.downcase == "y"
     db.exec_params("INSERT INTO links (name,url) VALUES ('$1','$2')",[slug,url])
     puts "Done!\n"
   else
