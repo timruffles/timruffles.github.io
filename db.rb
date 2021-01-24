67d6b00a37a3e8cee76202d9e45a951328a7406b
timruffles <timruffles@users.noreply.github.com>
2021-01-24 15:01:49 +0000

    deploy: a1b7da7328867316ffaafd7465c26c1a026db431

--- db.rb
+++
@@ -1,8 +0,0 @@
-require "uri"
-require "pg"
-def get_db
-  u = URI.parse(ENV["DATABASE_URL"])
-  conn = ["host","port","user","password"].inject({}) {|h,k| h[k] = u.send(k) ; h}
-  conn["dbname"] = u.path[1..-1]
-  PG.connect(conn)
-end
